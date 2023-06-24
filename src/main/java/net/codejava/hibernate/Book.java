b39f6deb748871dfe1071d00503806361d73e9df
Kiran Jagtap <karan27288@fmail.com>
2023-06-24 17:26:59 +0530

    First hello commit

---
+++ src/main/java/net/codejava/hibernate/Book.java
@@ -0,0 +1,55 @@
+package net.codejava.hibernate;
+
+public class Book {
+    private long id;
+
+    private String title;
+    private String author;
+    private float price;
+    public Book() {
+    }
+ 
+    
+    public Book(long id, String title, String author, float price) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+
+
+    public long getId() {
+        return id;
+    }
+ 
+    public void setId(long id) {
+        this.id = id;
+    }
+ 
+    public String getTitle() {
+        return title;
+    }
+ 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+ 
+    public String getAuthor() {
+        return author;
+    }
+ 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+ 
+    public float getPrice() {
+        return price;
+    }
+ 
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+
+}
